05e544312cb58ca9a7db885049c31b9bfa01d045
Brielle Linna <70108865+BrielleLinna@users.noreply.github.com>
2020-09-16 08:37:58 -0700

    Add files via upload

---
+++ Basic Javascript Projects/main.js
@@ -0,0 +1,19 @@
+document.write("\"Be who you are and say what you feel,"
++"because those who mind don\'t matter and those who matter don\'t mind.\""
++"-Dr. Suess");
+
+var appetizer="salad", dinner="lasanga", drink="white wine", dessert="cheesecake";
+document.write(dessert);
+
+var appetizer=appetizer.fontcolor("purple");
+var dinner=dinner.fontcolor("orange");
+var drink=drink.fontcolor("blue");
+var dessert=dessert.fontcolor("pink");
+
+
+var dinner="lasanga"
+var result=dinner.fontcolor("blue");
+
+document.write(dinner); //Writing a variable result
+
+document.write(3+6); //Writing a function
