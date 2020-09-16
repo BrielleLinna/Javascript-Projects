748d36daea24dd91b2716a9b982bdf1f13a79d8e
Brielle Linna <70108865+BrielleLinna@users.noreply.github.com>
2020-09-16 13:57:26 -0700

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,10 @@
+document.write(3+9+1)
+
+var Y="hot" //Assigning first variable a string value
+var X="dog" //Assigning second variable a string value
+var Z=Y + X //concatenating the two string values
+document.write(Z)
+
+var green="Green is my favorite color"; 
+        var green=green.fontcolor("#32CD32"); //Assigning a variable a font color using the fontcolor method
+        document.write(green);
