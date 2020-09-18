7d3612370670824278125dbb0663bf29983700ab
Brielle Linna <70108865+BrielleLinna@users.noreply.github.com>
2020-09-18 14:49:43 -0700

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,4 @@
+
+txt1 = "It is a rainy "; //creating a string
+txt1 += "day in Portland"; //using += operator for another string
+document.getElementById("test").innerHTML = txt1; //calling html to display the function
