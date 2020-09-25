3ef9cf8d15294575b1334a88d3a3b767638450c4
Brielle Linna <70108865+BrielleLinna@users.noreply.github.com>
2020-09-25 10:17:47 -0700

    Add files via upload

---
+++ Project3_math_operators/JS/main.js
@@ -0,0 +1,15 @@
+function negation_Operator() {
+    var x= 10;
+    document.getElementById("Math").innerHTML = -x;
+}
+
+
+function more_Math() {
+    var simple_Math= (1+2)*10/2-5;
+    document.getElementById("Math").innerHTML="1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals" + simple_Math;
+}
+
+function subtraction_Function() {
+    var subtraction= 5 -2;
+    document.getElementById("Math").innerHTML= "5-2=" + subtraction;
+}
