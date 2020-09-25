cc4d7166382db43ba8c4970ee82c4e827e6f7fb7
Brielle Linna <70108865+BrielleLinna@users.noreply.github.com>
2020-09-25 11:57:55 -0700

    Add files via upload

---
+++ Project4_dictionaries/JS/main.js
@@ -0,0 +1,33 @@
+function my_Dictionary() {
+    var Animal = {
+        Species:"Dog",
+        Color:"Black",
+        Breed:"Labrador",
+        Age:5,
+        Sound:"Bark!"
+    };
+    document.getElementById("Dictionary").innerHTML = Animal.Sound;
+}
+
+function my_Dictionary() {
+    var Animal = {
+        Species:"Dog",
+        Color:"Black",
+        Breed:"Labrador",
+        Age:5,
+        Sound:"Bark!"
+    };
+    delete Animal.Sound;
+    document.getElementById("Dictionary").innerHTML = Animal.Sound;
+    9+6
+}
+
+function my_Dictionary1() {
+    var Car = {
+        Model:"Mercades",
+        Color:"White",
+        Year:2014,
+     };
+     document.getElementById("Dictionary1").innerhtml = Car.Color;
+}
+
